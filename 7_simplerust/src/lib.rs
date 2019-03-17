4f0e2c6503779bb73a48f872f3c9369b54e1c255
umorigu <umorigu@gmail.com>
2019-03-17 22:44:39 +0000

    Add CreateCommand extern and SimpleRust_RedisCommand

--- 7_simplerust/src/lib.rs
+++ 7_simplerust/src/lib.rs
@@ -3,6 +3,9 @@ use libc::c_int;
 
 const MODULE_NAME: &'static str = "emptyrust2";
 
+pub enum RedisModuleCtx {}
+pub enum RedisModuleString {}
+
 #[allow(improper_ctypes)]
 #[link(name = "redismodule", kind = "static")]
 extern "C" {
@@ -14,8 +17,38 @@ extern "C" {
     ) -> c_int;
 }
 
-pub enum RedisModuleCtx {}
-pub enum RedisModuleString {}
+pub fn SimpleRust_RedisCommand(
+    ctx: *mut RedisModuleCtx,
+    argv: *mut *mut RedisModuleString,
+    argc: c_int,
+) -> c_int {
+    if (argc != 2) {
+        return 1;
+    }
+    return 0;
+}
+
+extern "C" {
+    // int RedisModule_CreateCommand(RedisModuleCtx *ctx, const char *name,
+    //   RedisModuleCmdFunc cmdfunc, const char *strflags, int firstkey,
+    //   int lastkey, int keystep);
+    fn RedisModule_CreateCommand(
+        ctx: *mut RedisModuleCtx,
+        name: *const u8,
+        fmdfunc: extern "C" fn(*mut RedisModuleCtx, *mut *mut RedisModuleString, c_int) -> c_int,
+        strflags: *const u8,
+        firstkey: c_int,
+        lastkey: c_int,
+        keystep: c_int,
+    );
+}
+
+/*
+enum Status {
+    Ok = 0,
+    Err = 1,
+}
+*/
 
 #[no_mangle]
 pub extern "C" fn RedisModule_OnLoad(
